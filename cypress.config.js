433709e911b1c79a7d1da31ad1329f9182909169
Victor Berton <bertonvictor@hotmail.com>
2024-10-01 21:16:50 -0300

    global variables configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   e2e: {
+    env: {
+      D1: 'https://marathonb2b.c5tq2wfczz-wegequipa1-d1-public.model-t.cc.commerce.ondemand.com',
+      S1: 'https://marathonb2b.c5tq2wfczz-wegequipa1-d1-public.model-t.cc.commerce.ondemand.com',
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
